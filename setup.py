23a9bef71baa028e9ab01d3ad6ac3ccf0a1c4f57
Limaitalos <123746792+Limaitalos@users.noreply.github.com>
2024-10-17 11:10:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with open("requirements.txt") as f:
 setup(
     name="package_name",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Ítalo",
+    author_email="italolima@yahoo.com.br",
+    description="menu_feijoada_restaurant",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Limaitalos/simple_package_template"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
